337310305c25d2c8ef4431000068e818d4864958
madhavan-developer <madhavangopideveloper@gmail.com>
2026-03-17 13:28:09 +0530

    new push

--- server.js
+++ server.js
@@ -30,14 +30,10 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(morgan("dev"));
